e96a5461f2ed431e8536685fce63ef6e3d59f7bc
Jeremy Warner <jeremywrnr@gmail.com>
2018-01-22 15:29:44 -0500

    no more codeclimate :/

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require_relative "../lib/open-remote.rb"
 
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+#require "codeclimate-test-reporter"
+#CodeClimate::TestReporter.start
 
 # testing sys output
 def puts(*x) x.join end
